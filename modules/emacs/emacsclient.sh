bff9e32646030728dc3ac4c2e91a3fd8b00ea937
John Allen <jallen@fb.com>
2017-04-08 16:44:28 -0700

    maybe?

--- modules/emacs/emacsclient.sh
+++ modules/emacs/emacsclient.sh
@@ -2,13 +2,21 @@
 
 _emacsfun()
 {
-    # get list of available X windows.
-    x=`emacsclient --alternate-editor '' --eval '(> 2 (safe-length (visible-frame-list)))' 2>/dev/null`
-    
-    if [ -z "$x" ] || [ "$x" = "nil" ] ;then
-	emacsclient -a emacs --no-wait "$@"
+    x=`emacsclient --alternate-editor '' --eval '(> 2 (safe-length (visible-frame-list)))' 2>/dev/null`    
+
+    if [ -z "$x" ] || [ "$x" = "nil" ]; then
+	# puth this file on the existing frame
+	emacsclient -a '' --no-wait "$@"
     else
-	emacsclient -a emacs --no-wait --create-frame "$@"
+	if [ $DIPLAY ]; then
+	    # create a frame, but don't wait for it. It'll be X11
+	    emacsclient -a '' --no-wait --create-frame "$@"
+	else
+	    # create an emacs frame interactively
+	    echo -e '\033kemacs\033\\'
+	    emacsclient -a '' --create-frame "$@"
+	    echo -e '\033kohno\033\\'
+	fi
     fi
 }
 
