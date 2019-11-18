fed192e932b017488ea20a0bd2b90e7f292bef72
John Allen <jallen@fb.com>
2019-11-18 12:15:48 -0500

    cp

--- modules/emacs/emacsclient.sh
+++ modules/emacs/emacsclient.sh
@@ -2,21 +2,20 @@
 
 _emacsfun()
 {
-    x=`emacsclient --alternate-editor '' --eval '(> 2 (safe-length (visible-frame-list)))' 2>/dev/null`    
+    x=`emacsclient --alternate-editor '' --eval '(> 2 (safe-length (visible-frame-list)))' 2>/dev/null`
 
     if [ -z "$x" ] || [ "$x" = "nil" ]; then
-	# puth this file on the existing frame
-	emacsclient -a '' --no-wait "$@"
+        # puth this file on the existing frame
+        emacsclient -a '' --no-wait "$@"
     else
-	if [ $DISPLAY ]; then
-	    # create a frame, but don't wait for it. It'll be X11
-	    emacsclient -a '' --no-wait --create-frame "$@"
-	else
-	    # create an emacs frame interactively
-	    emacsclient -a '' --create-frame "$@"
-	fi
+        if [ $DISPLAY ]; then
+            # create a frame, but don't wait for it. It'll be X11
+            emacsclient -a '' --no-wait --create-frame "$@"
+        else
+            # create an emacs frame interactively
+            emacsclient -a '' --create-frame "$@"
+        fi
     fi
 }
 
 _emacsfun "$@"
-
